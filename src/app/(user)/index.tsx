9fcfac5556e5994130416ba9acfa03da2445dbbf
Daniil Leshchev <leshev.da@gmail.com>
2024-04-17 15:22:07 +0500

    gradient background

--- src/app/(user)/index.tsx
+++ src/app/(user)/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Text from "@/components/StyledText";
-
+import { LinearGradient } from 'expo-linear-gradient';
+import Drawer from 'expo-router/drawer';
+import Button from '@/components/Button';
+const gradientColors = ['#9FA1E3', '#19287A']
 export default function MainScreen() {
   return (
-    <View>
-      <Text>Main Screen</Text>
-    </View>
+      <LinearGradient
+        style={styles.gradient}
+        colors={gradientColors}
+      >
+        <Drawer.Screen options={{headerShown: false}}/>
+      </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+  }
+})
