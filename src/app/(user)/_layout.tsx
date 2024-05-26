e908226634a0c469aabe33441bec6dde8e775260
Daniil Leshchev <leshev.da@gmail.com>
2024-05-27 01:02:20 +0500

    change file structure for sign-out

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -140,7 +140,7 @@ export default function MenuStack() {
         />
 
         <Drawer.Screen
-          name="sign-out/index"
+          name="sign-out"
           options={{
             drawerLabel: 'Выйти', 
             title: '',
