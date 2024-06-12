6434535b0eb99a1fa6b58fe0938b2adf5e37ab56
Daniil Leshchev <leshev.da@gmail.com>
2024-06-12 20:20:29 +0500

    add schedule parser

---
+++ src/modeus/parser.ts
@@ -0,0 +1,35 @@
+import moment from 'moment';
+import 'moment/locale/ru';
+export const parseICS = (icsString: string) => {
+  const lines = icsString.split('\n');
+  const events = [];
+
+  let event: { [key: string]: string } = {};
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    if (line === 'BEGIN:VEVENT')
+      event = {};
+    else if (line === 'END:VEVENT')
+      events.push(event);
+    else if (event) {
+      const match = /^(LOCATION|CATEGORIES):(.*)$/.exec(line);
+      const dateMatch = /^(DTSTART|DTEND).*:(.*)$/.exec(line);
+
+      if (match) {
+        const [_, key, value] = match;
+        if (key === 'CATEGORIES')
+            event['title'] = value;
+        else
+            event[key.toLowerCase()] = value;
+      }
+
+      if (dateMatch) {
+        const [_, key, value] = dateMatch;
+        event[key.substring(2).toLowerCase()] = moment(value).format('YYYY-MM-DD HH:mm');
+      }
+    }
+  }
+
+  return events;
+}
